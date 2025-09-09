68eede634ce7c8aab3906570b671f3c2ca787990
murtazayevbahodir6-ux <murtazayevbaxodir6@gmail.com>
2025-09-10 00:35:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from api.vacancy_loader import VacancyLoader
+
+import logging
+
+
+def main():
+    try:
+        loader = VacancyLoader()
+        loader.fit()
+    except Exception as err:
+        logging.error(err)
 
