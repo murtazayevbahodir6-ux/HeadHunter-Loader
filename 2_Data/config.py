77391cdf8dc39776e1ab7980bb9f2df3c6b00165
murtazayevbahodir6-ux <murtazayevbaxodir6@gmail.com>
2025-09-09 23:44:11 +0500

    Update config.py

--- 2_Data/config.py
+++ 2_Data/config.py
@@ -1,1 +1,11 @@
+import environs
 
+env = environs.Env()
+env.read_env()
+
+HOST = env.str("HOST", "localhost")
+USER = env.str("DB_USER")
+PASSWORD = env.str("PASSWORD")
+DB = env.str("DB")
+
+BASE_URL = "https://api.hh.ru/vacancies"
