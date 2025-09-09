d97f33f71625250a8b41ea085430753a56e2ad51
murtazayevbahodir6-ux <murtazayevbaxodir6@gmail.com>
2025-09-10 00:35:06 +0500

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,1 +1,9 @@
+import os
+from data.config import HOST, USER, PASSWORD, DB
+import logging
+from db.database import Database
 
+
+db = Database(
+    host=HOST, user=USER, password=PASSWORD, db=DB
+)
